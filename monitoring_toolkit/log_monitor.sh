961dfa0d2cc00c75ae4ffdb9a302a3c87d834eeb
LucaGavra <100366930+LucaGavra@users.noreply.github.com>
2025-03-22 23:22:21 +0200

    Update log_monitor.sh

--- monitoring_toolkit/log_monitor.sh
+++ monitoring_toolkit/log_monitor.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Define log file path
-LOG_FILE="/var/log/syslog"  # Change to /var/log/messages on RHEL-based systems
+LOG_FILE="/var/log/syslog"  
 ALERT_LOG="anomaly.log"
 
 echo "Monitoring Logs for Errors..."
 echo "---- Log Monitoring Started at $(date) ----" >> $ALERT_LOG
 
-# Monitor logs for errors in real-time
 tail -n 100 -F $LOG_FILE | grep --line-buffered -i "error\|fail\|critical\|panic" | while read line
 do
     echo "[$(date)] ALERT: $line"
